3da5a4e9f5890181592d8d5e25f19ec2c052ad84
Adam Jacob Muller <adam@isprime.com>
2015-02-22 01:22:12 -0500

    track cases enumerations in the database so we don't repeat if something breaks
    load a list of courts from courts.json with the FIPS code and enumerate
    through all courts, not just one

--- enumerate-cases.py
+++ enumerate-cases.py
@@ -6,6 +6,7 @@ import psycopg2
 import sys
 import logging
 import httplib
+import datetime
 
 from config import *
 
@@ -22,24 +23,39 @@ if False:
 conn = psycopg2.connect(DB_CONNECTION_STRING)
 cur = conn.cursor()
 
-dates = [
-]
+courts_fh = open("courts.json", "r")
+courts = json.load(courts_fh)
 
-for m in xrange(1, 13):
-    for d in xrange(1, 31):
-        dates.append((2014, m, d))
+dates = []
+
+for court in courts:
+    for m in xrange(1, 3):
+        for d in xrange(1, 31):
+            dates.append((2015, m, d, court['fipsCode'], court['name']))
 
 for t_date in dates:
     date = '%02d/%02d/%04d' % (t_date[1], t_date[2], t_date[0])
     pdate = '%04d-%02d-%02d' % (t_date[0], t_date[1], t_date[2])
 
+    try:
+        cur.execute("select case_track_id from case_track where fipsCode=%s and date=%s", [t_date[3], pdate])
+    except psycopg2.DataError:
+        print "ghetto date validation says %s is crazy" % pdate
+        conn.rollback()
+        continue
+    if cur.rowcount == 1:
+        print "SKIPPING %s %s" % (t_date[4], pdate)
+        continue
+
     page = 1
+    cases = 0
+    started = datetime.datetime.now()
 
     while True:
         data = {
-            'curentFipsCode': '025',
+            'curentFipsCode': "%03d" % t_date[3],
             'searchTerm': date,
-            'searchFipsCode': '025',
+            'searchFipsCode': "%03d" % t_date[3],
         }
         if page == 1:
             data['caseSearch'] = 'Search'
@@ -50,9 +66,9 @@ for t_date in dates:
             if 'caseSearch' in data:
                 del data['caseSearch']
 
-        result = requests.post('https://eapps.courts.state.va.us/gdcourts/caseSearch.do', data = data, cookies = cookies)
+        result = requests.post('https://eapps.courts.state.va.us/gdcourts/caseSearch.do', data=data, cookies=cookies)
 
-        print "%s %s %s %s" % (result, pdate, page, len(result.text))
+        print "%s %s %s %s %s" % (t_date[4], result, pdate, page, len(result.text))
 
         if 'You have exceeded the maximum number of requests allowed for a given time period' in result.text:
             print "> You have exceeded the maximum number of requests allowed for a given time period"
@@ -66,11 +82,11 @@ for t_date in dates:
 
         g_d = re.findall("displayCaseNumber=([A-Z0-9-]+)&", result.text)
 
-        x = open("raw/html/r-%s-%s.html" % (pdate, page), "w")
+        x = open("raw/html/r-%03d-%s-%s.html" % (t_date[3], pdate, page), "w")
         x.write(result.text.encode("ascii", errors='ignore'))
         x.close()
 
-        x = open("raw/cases-json/r-%s-%s.json" % (pdate, page), "w")
+        x = open("raw/cases-json/r-%03d-%s-%s.json" % (t_date[3], pdate, page), "w")
         json.dump(g_d, x)
         x.close()
 
@@ -79,10 +95,19 @@ for t_date in dates:
             break
 
         for case in g_d:
+            cases += 1
             cur.execute("update cases set last=now() where casenumber=%s", [case])
             if cur.rowcount == 0:
-                cur.execute("insert into cases (casenumber,first,last) values (%s,now(),now())", [case])
+                cur.execute("insert into cases (casenumber,fipsCode,first,last) values (%s,%s,now(),now())", [case, t_date[3]])
 
         conn.commit()
 
         page += 1
+    try:
+        cur.execute("insert into case_track (date,fipsCode,started,completed,pages,cases) values (%s,%s,%s,%s,%s,%s)", [
+            pdate, t_date[3], started, datetime.datetime.now(), page - 1, cases
+        ])
+        conn.commit()
+    except psycopg2.DataError:
+        conn.rollback()
+        continue
